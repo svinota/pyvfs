4900d8028a13fce22d8b233e7e1c254fbfaf9b7e
Peter V. Saveliev <peet@redhat.com>
2012-09-22 15:54:01 +0200

    use custom Inode class to monitor objects

--- objectfs/pyfs.py
+++ objectfs/pyfs.py
@@ -25,6 +25,35 @@ File.register(types.LongType)
 File.register(types.StringType)
 File.register(types.UnicodeType)
 
+class vInode(Inode):
+
+    def register(self, obj):
+        self.observe = obj
+
+    def sync(self):
+        if not hasattr(self, "observe"):
+            return
+
+        if self.mode & stat.S_IFDIR:
+            chs = set(self.children.keys())
+            obs = set(dir(self.observe))
+            to_delete = chs - obs - set(self.special_names)
+            to_create = obs - chs
+            for i in to_delete:
+                self.storage.remove(self.children[i].path)
+            # consider for saving stack in the root object inode!
+            for i in to_create:
+                self.storage.create(getattr(self.observe, i),
+                        parent=self, name=i)
+            # sync observe objects of children
+            for i in dir(self.observe):
+                if self.children.has_key(i):
+                    self.children[i].observe = getattr(self.observe, i)
+        else:
+            self.seek(0)
+            self.truncate()
+            self.write(str(getattr(self.parent.observe, self.name)))
+
 class PyFS(Storage):
 
     def create(self, obj, parent=None, name=None, stack=None):
@@ -34,29 +63,38 @@ class PyFS(Storage):
         if not stack:
             stack = {}
 
+        if not name:
+            name = repr(obj)
+
         if obj in stack.keys():
             return
 
+        if isinstance(obj, Skip):
+            return
+
+        if name.startswith("_"):
+            return
+
         if isinstance(obj, File):
-            if not name:
-                name = str(hash(obj))
             new = parent.create(name)
-            new.write(str(obj))
+            new.register(obj)
         else:
             try:
                 stack[obj] = True
-                new = parent.create(repr(obj).strip("<>"), mode=stat.S_IFDIR)
+                new = parent.create(name, mode=stat.S_IFDIR)
+                new.register(obj)
                 for item in dir(obj):
-                    if item.startswith('_') or isinstance(getattr(obj, item), Skip):
-                        continue
                     self.create(getattr(obj, item), new, item, stack)
             except:
                 return
 
         return new
 
-pyfs = PyFS()
-srv = py9p.Server(listen=('127.0.0.1', 8000), chatty=False, dotu=True)
+# import rpdb2
+# rpdb2.start_embedded_debugger("bala", fAllowRemote=True)
+
+pyfs = PyFS(vInode)
+srv = py9p.Server(listen=('127.0.0.1', 8000), chatty=True, dotu=True)
 srv.mount(v9fs(pyfs))
 srv_thread = Thread(target=srv.serve)
 srv_thread.setDaemon(True)
