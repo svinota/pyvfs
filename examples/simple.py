6d26a49b066ead60ed1f919f7c82c58033c4bec6
Peter V. Saveliev <peet@redhat.com>
2012-10-01 16:02:27 +0200

    extend the example and add comments

--- examples/simple.py
+++ examples/simple.py
@@ -3,20 +3,69 @@
 Simple PyFS example
 """
 
-# start PyFS thread and import decorator
+# start PyFS thread and import the decorator
 from objectfs.pyfs import export
 
 
 # export all objects of the Example class
-@export
+# do not export "boo" atributes (see Child class)
+@export(blacklist=["/boo"])
 class Example(object):
 
     def __init__(self, text):
+        """
+        PyFS @export decorator substitutes the constructor
+        with wrapped function, that creates weakref.proxy()
+        to the object.
+        """
         self.text = text
 
+    def __repr__(self):
+        """
+        Files inside the object tree are named after attributes
+        names, but the tree root is named automatically. If you
+        want, you can change the naming with __repr__()
+        """
+        return "%s(%s) at 0x%x" % (self.__class__.__name__,
+                self.text, id(self))
+
+
+# please note, that objects of derived classes will be
+# also exported
+class Child(Example):
+
+    def __init__(self, text):
+        """
+        The most tricky thing is that in this case the object
+        will be exported by parent's __init__(), and just after
+        creation it will not have all the attributes and,
+        possibly, will not be ready for __repr__(). But we don't
+        care, 'cause it is sync'ed in runtime, and in the case
+        of failing __repr__() it will be renamed later -- you
+        will not even notice that unless you will read the log
+        """
+        Example.__init__(self, text)
+        self.id = id(self)
+        # this one will not be exported, 'cause it is
+        # blacklisted
+        self.boo = True
+
+    def __repr__(self):
+        """
+        This __repr__() will fail at the time of object's
+        export (see above), so it will cause the object's
+        tree root to be renamed. To look at this you should
+        export env variable PYFS_LOG=True and cat .../log
+        """
+        return "%s [0x%x]" % (self.__class__.__name__, self.id)
 
 # spawn several objects
-objects = [Example(x) for x in range(10)]
+objects_A = [Example(x) for x in range(5)]
+objects_B = [Child(x) for x in range(2)]
+
+# now you can mount your script with the command
+# mount -t 9p -o ro,port=10001 127.0.0.1 /mnt
 
 # wait for input
+# W: do not forget to umount! :)
 raw_input("press enter to exit >")
