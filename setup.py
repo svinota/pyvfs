cea18a9e6cee8ba6c0917403666e0cc9e0f68771
Peter V. Saveliev <peet@redhat.com>
2012-10-06 18:10:58 +0200

    version/spec update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 # bump version
 setup(name='pyvfs',
-    version='0.2.1',
+    version='0.2.2',
     description='Python VFS library',
     author='Peter V. Saveliev',
     author_email='peet@altlinux.org',
