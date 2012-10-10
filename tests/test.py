052eb53722bae70e88da27ea24233ddec33fa9ce
Peter V. Saveliev <peet@redhat.com>
2012-10-10 18:57:41 +0200

    update testcase

--- tests/test.py
+++ tests/test.py
@@ -23,16 +23,31 @@ class Bala(object):
                 1024,
                 ]
 
+    def __repr__(self):
+        return str(id(self))
+
+    def _get_bala(self):
+        return self.__bala
+
+    def _set_bala(self, value):
+        self.__bala = value
+
+    bala = property(_get_bala, _set_bala)
+
+    def print_bala(self):
+        print self.bala
+
 e = []
 
-while True:
-    a = raw_input("$ ")
-    if a == "pop":
-        e.pop()
-    elif a == "gc":
-        gc.collect()
-    elif a == "ls":
-        print e
-        print [x for x in gc.get_objects() if isinstance(x, Bala)]
-    else:
-        e.append(Bala(a))
+if __name__ == "__main__":
+    while True:
+        a = raw_input("$ ")
+        if a == "pop":
+            e.pop()
+        elif a == "gc":
+            gc.collect()
+        elif a == "ls":
+            print e
+            print [x for x in gc.get_objects() if isinstance(x, Bala)]
+        else:
+            e.append(Bala(a))
