6ee7255c85e403c4b60d8bbc76f035b3573bdf85
Peter V. Saveliev <peet@redhat.com>
2012-10-19 22:58:18 +0200

    0.2.5-alt1: version update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 # bump version
 setup(name='pyvfs',
-    version='0.2.4',
+    version='0.2.5',
     description='Simple Python VFS module',
     author='Peter V. Saveliev',
     author_email='peet@redhat.com',
