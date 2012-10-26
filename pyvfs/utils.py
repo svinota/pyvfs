5b43b521005d2a7cb461d1cd6196f07ff50b9913
Peter V. Saveliev <peet@redhat.com>
2012-10-26 17:44:10 +0200

    vfs Server: authentication options
    * allow_root FUSE option
    * allow_other FUSE option
    * authmode 9p option
    * keyfiles 9p option

--- pyvfs/utils.py
+++ pyvfs/utils.py
@@ -5,6 +5,7 @@ pyvfs.utils -- utility classes
 Utility classes for VFS
 """
 import os
+import ast
 import logging
 import threading
 from collections import deque
@@ -122,6 +123,10 @@ class Server(threading.Thread):
             "mountpoint": "./mnt",
             "debug": False,
             "log": False,
+            "allow_root": False,
+            "allow_other": False,
+            "authmode": "",
+            "keyfiles": {},
             }
 
     def __init__(self, fs):
@@ -134,9 +139,13 @@ class Server(threading.Thread):
             if isinstance(k, bool):
                 value = value.lower() in (
                         "yes", "true", "on", "t", "1")
+            elif isinstance(k, dict):
+                value = ast.literal_eval(value)
             elif not isinstance(k, str):
                 value = type(k)(value)
             setattr(self, i, value)
+        if self.authmode == "":
+            self.authmode = None
         for i in list(self.protocols.keys()):
             if i not in protocols:
                 del self.protocols[i]
@@ -162,6 +171,7 @@ class Server(threading.Thread):
 
     def mount_v9fs(self):
         srv = py9p.Server(listen=(self.address, self.port),
+                authmode=self.authmode, key=self.keyfiles,
                 chatty=self.debug, dotu=True)
         srv.mount(v9fs(self.fs))
         return srv.serve
@@ -172,6 +182,10 @@ class Server(threading.Thread):
         srv.fuse_args.setmod('foreground')
         if self.debug:
             srv.fuse_args.add('debug')
+        if self.allow_root:
+            srv.fuse_args.add('allow_root')
+        if self.allow_other:
+            srv.fuse_args.add('allow_other')
         srv.fuse_args.mountpoint = os.path.realpath(self.mountpoint)
         return srv.main
 
